fa02ff9536ee5e28674aac2f277f8c67d428b35b
Mehmet <mehmetzahir5834@gmail.com>
2025-03-05 03:04:23 +0300

    #Created New Project

---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string? Name { get; set; }
+        public string? Mail { get; set; }
+        public string? MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Portfollio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfollio
+    {
+        public int PortfollioID { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
